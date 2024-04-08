62d8e150ebd206f45b2dc06ef6cfe32d79a6d7e9
Matheus <matheus.silva@iob.com.br>
2024-04-08 18:27:28 -0300

    criando objeto com dados da temporada

--- src/main/java/br/com/dopoke/zintv/ZintvApplication.java
+++ src/main/java/br/com/dopoke/zintv/ZintvApplication.java
@@ -1,5 +1,7 @@
 package br.com.dopoke.zintv;
 
+import br.com.dopoke.zintv.model.DadosEpisodio;
+import br.com.dopoke.zintv.model.DadosTemporada;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import br.com.dopoke.zintv.model.DadosSeries;
 import br.com.dopoke.zintv.service.ConsumoAPI;
 import br.com.dopoke.zintv.service.ConverterDados;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ZintvApplication implements CommandLineRunner{
@@ -18,17 +23,32 @@ public class ZintvApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
+
 		var json = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt2467372&apikey=f165582f");
 		var jsonbb = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt0903747&apikey=f165582f");
-		
-		System.out.println(json);
+		var jsonep = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt0903747&season=1&episode=2&apikey=f165582f");
+		var jsonss = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt090377&season=1&episode=2&apikey=f165582f");
 		
 		ConverterDados conversor = new ConverterDados();
-		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
 
+		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
 		DadosSeries dadosbb = conversor.obterDados(jsonbb, DadosSeries.class);
+		DadosEpisodio dadosep = conversor.obterDados(jsonep, DadosEpisodio.class);
+		DadosTemporada dadosss = conversor.obterDados(jsonss, DadosTemporada.class);
+
 		System.out.println(dados);
 		System.out.println(dadosbb);
+		System.out.println(dadosep);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+
+		for (int i = 1; i<=dadosbb.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt0903747&season=" +i+ "&apikey=f165582f");
+			DadosTemporada dadostp = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadostp);
+		}
+		temporadas.forEach(System.out::println);
 	}
 
 }
