6bb32a1e12e6248ecbeb93c77cce2e42d9e576a0
Matheus <matheus.silva@iob.com.br>
2024-04-08 10:10:09 -0300

    adicionando novos dados

--- src/main/java/br/com/dopoke/zintv/ZintvApplication.java
+++ src/main/java/br/com/dopoke/zintv/ZintvApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.dopoke.model.DadosSeries;
+import br.com.dopoke.zintv.model.DadosSeries;
 import br.com.dopoke.zintv.service.ConsumoAPI;
 import br.com.dopoke.zintv.service.ConverterDados;
 
@@ -19,14 +19,16 @@ public class ZintvApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
 		var json = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt2467372&apikey=f165582f");
+		var jsonbb = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt0903747&apikey=f165582f");
 		
 		System.out.println(json);
 		
 		ConverterDados conversor = new ConverterDados();
 		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
-		
+
+		DadosSeries dadosbb = conversor.obterDados(jsonbb, DadosSeries.class);
 		System.out.println(dados);
-		
+		System.out.println(dadosbb);
 	}
 
 }
--- src/main/java/br/com/dopoke/zintv/model/DadosSeries.java
+++ src/main/java/br/com/dopoke/zintv/model/DadosSeries.java
@@ -1,4 +1,4 @@
-package br.com.dopoke.model;
+package br.com.dopoke.zintv.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
