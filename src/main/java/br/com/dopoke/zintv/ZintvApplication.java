aa6b383a016b9892db67327f8d8fc81ed8d6f6af
Matheus Salustiano <matheustechads@hmail.com>
2024-04-10 07:02:43 -0300

    clean class run para metodo principal

--- src/main/java/br/com/dopoke/zintv/ZintvApplication.java
+++ src/main/java/br/com/dopoke/zintv/ZintvApplication.java
@@ -1,17 +1,10 @@
 package br.com.dopoke.zintv;
 
-import br.com.dopoke.zintv.model.DadosEpisodio;
-import br.com.dopoke.zintv.model.DadosTemporada;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.dopoke.zintv.model.DadosSeries;
-import br.com.dopoke.zintv.service.ConsumoAPI;
-import br.com.dopoke.zintv.service.ConverterDados;
-
-import java.util.ArrayList;
-import java.util.List;
+import br.com.dopoke.zintv.principal.Principal;
 
 @SpringBootApplication
 public class ZintvApplication implements CommandLineRunner {
@@ -22,32 +15,8 @@ public class ZintvApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt2467372&apikey=f165582f");
-		var jsonbb = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt0903747&apikey=f165582f");
-		var jsonep = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt0903747&season=1&episode=2&apikey=f165582f");
-		var jsonss = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt090377&season=1&episode=2&apikey=f165582f");
-
-		ConverterDados conversor = new ConverterDados();
-
-		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
-		DadosSeries dadosbb = conversor.obterDados(jsonbb, DadosSeries.class);
-		DadosEpisodio dadosep = conversor.obterDados(jsonep, DadosEpisodio.class);
-		DadosTemporada dadosss = conversor.obterDados(jsonss, DadosTemporada.class);
-
-		System.out.println(dados);
-		System.out.println(dadosbb);
-		System.out.println(dadosep);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dadosbb.totalTemporadas(); i++) {
-			json = consumoAPI.obterDados("https://www.omdbapi.com/?i=tt0903747&season=" + i + "&apikey=f165582f");
-			DadosTemporada dadostp = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadostp);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 
 }
--- src/main/java/br/com/dopoke/zintv/principal/Principal.java
+++ src/main/java/br/com/dopoke/zintv/principal/Principal.java
@@ -1,22 +1,42 @@
 package br.com.dopoke.zintv.principal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import br.com.dopoke.zintv.model.DadosSeries;
+import br.com.dopoke.zintv.model.DadosTemporada;
 import br.com.dopoke.zintv.service.ConsumoAPI;
+import br.com.dopoke.zintv.service.ConverterDados;
 
 public class Principal {
 
 	private Scanner leitura = new Scanner(System.in);
 	private ConsumoAPI consumoAPI = new ConsumoAPI();
+	private ConverterDados conversor = new ConverterDados();
 
 	private final String ENDERECO = "https://www.omdbapi.com/?t=";
+	private final String SEASON = "&season=";
 	private final String API_KEY = "&apikey=f165582f";
 
 	public void exibeMenu() {
+
 		System.out.println("Digite o nome da serie para busca: ");
 		var nomeSerie = leitura.nextLine();
 
 		var json = consumoAPI.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+		
+		DadosSeries dadosbb = conversor.obterDados(json, DadosSeries.class);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dadosbb.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + SEASON + i + API_KEY);
+			DadosTemporada dadostp = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadostp);
+		}
+		temporadas.forEach(System.out::println);
+		temporadas.forEach(t -> t.episodeos().forEach(e -> System.out.println(e.titulo())));
 	}
 
 }
